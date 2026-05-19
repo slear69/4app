008ac7af6c30112811b24d44e0dc47a5b1dcb510
hi_frombg:)ngl <dimitarp758@gmail.com>
2026-05-19 14:00:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -88,49 +88,93 @@ def find_harmful_ingredients(text):
 
     harmful_ingredients = {
 
-        # COLORANTS
-        "E102": "Colorant",
-        "E104": "Colorant",
-        "E110": "Colorant",
-        "E122": "Colorant",
-        "E123": "Colorant",
-        "E127": "Colorant",
-        "E131": "Colorant",
-        "E133": "Colorant",
-        "E151": "Colorant",
+        # -------------------
+        # COLORANTS / ОЦВЕТИТЕЛИ
+        # -------------------
+        "E102": "Colorant / Оцветител (Tartrazine)",
+        "E104": "Colorant / Оцветител",
+        "E110": "Colorant / Оцветител (Sunset Yellow)",
+        "E122": "Colorant / Оцветител (Azorubine)",
+        "E123": "Colorant / Оцветител",
+        "E124": "Colorant / Оцветител (Ponceau 4R)",
+        "E127": "Colorant / Оцветител",
+        "E129": "Colorant / Оцветител (Allura Red)",
+        "E131": "Colorant / Оцветител",
+        "E132": "Colorant / Оцветител",
+        "E133": "Colorant / Оцветител (Blue)",
+        "E142": "Colorant / Оцветител",
+        "E151": "Colorant / Оцветител (Black PN)",
 
-        # PRESERVATIVES
-        "E211": "Preservative",
-        "E250": "Preservative",
-        "E220": "Preservative",
-        "E221": "Preservative",
-        "E222": "Preservative",
-        "E223": "Preservative",
-        "E224": "Preservative",
-        "E225": "Preservative",
-        "E226": "Preservative",
-        "E227": "Preservative",
-        "E228": "Preservative",
+        # -------------------
+        # PRESERVATIVES / КОНСЕРВАНТИ
+        # -------------------
+        "E200": "Preservative / Консервант",
+        "E202": "Preservative / Консервант",
+        "E211": "Preservative / Консервант (Sodium benzoate)",
+        "E220": "Preservative / Консервант (Sulphites)",
+        "E221": "Preservative / Консервант",
+        "E222": "Preservative / Консервант",
+        "E223": "Preservative / Консервант",
+        "E224": "Preservative / Консервант",
+        "E225": "Preservative / Консервант",
+        "E226": "Preservative / Консервант",
+        "E227": "Preservative / Консервант",
+        "E228": "Preservative / Консервант",
+        "E250": "Preservative / Консервант (Sodium nitrite)",
+        "E251": "Preservative / Консервант (Nitrate)",
+        "E252": "Preservative / Консервант",
 
-        # SWEETENERS & FLAVOR ENHANCERS
-        "E621": "Flavor Enhancer",
-        "E951": "Sweetener",
-        "E420": "Sweetener",
+        # -------------------
+        # SWEETENERS / ПОДСЛАДИТЕЛИ
+        # -------------------
+        "E420": "Sweetener / Подсладител (Sorbitol)",
+        "E421": "Sweetener / Подсладител",
+        "E950": "Sweetener / Подсладител (Acesulfame K)",
+        "E951": "Sweetener / Подсладител (Aspartame)",
+        "E952": "Sweetener / Подсладител (Cyclamate)",
+        "E954": "Sweetener / Подсладител (Saccharin)",
+        "E955": "Sweetener / Подсладител (Sucralose)",
 
-        # STABILIZERS
-        "E320": "Stabilizer",
-        "E321": "Stabilizer",
-        "E407": "Stabilizer",
-        "E450": "Stabilizer",
+        # -------------------
+        # FLAVOR ENHANCERS / ОВКУСИТЕЛИ
+        # -------------------
+        "E621": "Flavor enhancer / Овкусител (MSG)",
+        "E627": "Flavor enhancer / Овкусител",
+        "E631": "Flavor enhancer / Овкусител",
+        "E635": "Flavor enhancer / Овкусител",
 
-        # OTHER INGREDIENTS
-        "palm oil": "Harmful Oil",
-        "палмово масло": "Вредно масло",
-        "aspartame": "Sweetener",
-        "аспартам": "Подсладител",
-        "monosodium glutamate": "Flavor Enhancer",
-        "sodium nitrate": "Preservative",
-        "high fructose corn syrup": "Sweetener"
+        # -------------------
+        # STABILIZERS / СТАБИЛИЗАТОРИ
+        # -------------------
+        "E320": "Stabilizer / Стабилизатор (BHA)",
+        "E321": "Stabilizer / Стабилизатор (BHT)",
+        "E407": "Stabilizer / Стабилизатор (Carrageenan)",
+        "E410": "Stabilizer / Стабилизатор",
+        "E412": "Stabilizer / Стабилизатор",
+        "E415": "Stabilizer / Стабилизатор",
+        "E440": "Stabilizer / Стабилизатор",
+        "E450": "Stabilizer / Стабилизатор (Phosphates)",
+
+        # -------------------
+        # OTHER HARMFUL / спорни съставки
+        # -------------------
+        "palm oil": "Harmful fat / Палмово масло",
+        "палмово масло": "Вредна мазнина / Палмово масло",
+
+        "hydrogenated oil": "Trans fat / Хидрогенирано масло",
+        "частично хидрогенирано": "Trans fat / Транс мазнини",
+
+        "high fructose corn syrup": "Sweetener / Високо фруктозен сироп",
+        "високо фруктозен сироп": "Sweetener / High fructose syrup",
+
+        "monosodium glutamate": "Flavor enhancer / Мононатриев глутамат",
+        "мононатриев глутамат": "Flavor enhancer / MSG",
+
+        "aspartame": "Sweetener / Аспартам",
+        "аспартам": "Sweetener / Аспартам",
+
+        "glucose-fructose syrup": "Sweetener / Глюкозо-фруктозен сироп",
+        "глюкозо-фруктозен сироп": "Sweetener / Glucose-fructose syrup"
     }
 
     found = []
@@ -138,12 +182,10 @@ def find_harmful_ingredients(text):
     text = text.lower()
 
     for ingredient, category in harmful_ingredients.items():
-
         if ingredient.lower() in text:
             found.append((ingredient, category))
 
     return found
-
 
 # ---------------------------
 # IMAGE INPUT
